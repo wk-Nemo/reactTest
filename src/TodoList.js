903a3a15a6626dc53d991acde3e9b806cde66200
wk-Nemo <2181544173@qq.com>
2021-03-22 17:47:53 +0800

    'todolist'

---
+++ src/TodoList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class TodoList extends Component {
+    render() {
+        return (
+            <div>
+                <input></input><button>提交</button>
+                <ul>
+                    <li>ajhd</li>
+                    <li>asjdhksahd</li>
+                </ul>
+            </div>
+        )
+    }
+}
+export default TodoList
