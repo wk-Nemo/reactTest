08ce0b93872a4f74f3353f38de08d51d9f0eeda3
wk-Nemo <2181544173@qq.com>
2021-03-23 17:46:00 +0800

    'value'

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,17 +1,44 @@
 import React, { Component, Fragment } from 'react';
 
 class TodoList extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            inputValue: 'asdasd',
+            list: []
+        }
+    }
+
+    // JSX语法
+    // 一些标签
+    // 绑定value
+    // 绑定事件--->修改this指向
+    // Fragment占位符
     render() {
         return (
             // 占位符
             <Fragment>
-                <input></input><button>提交</button>
+                <div>
+                    <input
+                     value={this.state.inputValue}
+                     onChange={this.handleInputChange.bind(this)}
+                    />
+                    <button>提交</button>
+                </div>
                 <ul>
                     <li>ajhd</li>
                     <li>asjdhksahd</li>
                 </ul>
             </Fragment>
         )
+    }
+    
+    // react中特有的设置值的方法，this.setState
+    handleInputChange(e) {
+        this.setState({
+            inputValue: e.target.value
+        })
     }
 }
 export default TodoList
